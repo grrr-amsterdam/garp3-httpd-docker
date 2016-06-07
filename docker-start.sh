c9f25b9a1ee313cdc177b60e72c73ba29b6146dd
David Spreekmeester <david@grrr.nl>
2016-06-07 18:02:30 +0200

    Simplified run command

--- docker-start.sh
+++ docker-start.sh
@@ -1,10 +1,11 @@
 eval (docker-machine env default)
-docker build -t grrr-amsterdam/garp3-docker .
-docker stop garp3
-docker rm garp3
-docker run -d --sig-proxy=false --name garp3 \
+#docker build -t grrr-amsterdam/garp3-docker .
+#docker stop garp3
+#docker rm garp3
+docker-compose up -d
+#docker run -d --sig-proxy=false --name garp3 \
 	#-v /Users/david/Sites/entree/application/data/mysql_data:/var/lib/mysql \
-	-p 80:80 \
-	-p 443:443 \
-	-p 3306:3306 \
-	grrr-amsterdam/garp3-docker &
+	#-p 80:80 \
+	#-p 443:443 \
+	#-p 3306:3306 \
+	#grrr-amsterdam/garp3-docker &
